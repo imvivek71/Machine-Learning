30a48dafd754f0f5df53e737628aa33a9ded62bc
imvivek71 <38568313+imvivek71@users.noreply.github.com>
2019-01-02 19:23:35 +0530

    Create ImportData.py
    #importing dataset

---
+++ Data Preprocessing/ImportData.py
@@ -0,0 +1,7 @@
+#importing dataset
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv("/home/vivek/Desktop/Data.csv")
+X = dataset.iloc[:,:-1].values
+Y = dataset.iloc[:,3].values    
