10dd23ad80dc634392e30abf4e5fdc6d9c062c32
Vivek Goswami <38568313+imvivek71@users.noreply.github.com>
2019-02-09 15:45:08 +0530

    ImportingLibraries

--- Data Preprocessing/ImportingLibraries.py
+++ Data Preprocessing/ImportingLibraries.py
@@ -4,6 +4,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+# Reading the dataset
 dataset = pd.read_csv("/home/vivek/Desktop/Data.csv")
 X = dataset.iloc[:,:-1].values
 Y = dataset.iloc[:,3].values    
