90a213cc696076c375ee91cc99b97bf2c4e62e58
Vivek Goswami <38568313+imvivek71@users.noreply.github.com>
2019-02-09 15:41:59 +0530

    DataPreprocessing

--- Data Preprocessing/DataPreprocessing.py
+++ Data Preprocessing/DataPreprocessing.py
@@ -36,7 +36,7 @@ y= LabelEncoder().fit_transform(y)
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train, y_test = train_test_split(x,y, test_size = 0.2, random_state=0)
 
-# Feature Scaling 
+# Feature Scaling of data
 
 from sklearn.preprocessing import StandardScaler
 sc_x =  StandardScaler()
